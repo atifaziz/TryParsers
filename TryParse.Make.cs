bda4a0ea24d2d9f7474335e7b3266f66dc0f78f9
Atif Aziz <code@raboof.com>
2018-02-01 19:00:03 +0100

    Delete trailing spaces

--- TryParse.Make.cs
+++ TryParse.Make.cs
@@ -3,31 +3,31 @@
 // TryParsers - TryParse from .NET Framework done right
 // Copyright (c) 2012, Atif Aziz. All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions are 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
 // met:
 //
-// - Redistributions of source code must retain the above copyright notice, 
+// - Redistributions of source code must retain the above copyright notice,
 //   this list of conditions and the following disclaimer.
 //
-// - Redistributions in binary form must reproduce the above copyright 
-//   notice, this list of conditions and the following disclaimer in the 
+// - Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
 //   documentation and/or other materials provided with the distribution.
 //
-// - The names of its contributors may be used to endorse or promote 
-//   products derived from this software without specific prior written 
+// - The names of its contributors may be used to endorse or promote
+//   products derived from this software without specific prior written
 //   permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER 
-// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 #endregion
@@ -51,7 +51,7 @@ namespace TryParsers
 
         /// <summary>
         /// Creates and returns a function that returns a user-defined
-        /// default value whenever the supplied parser function throws an 
+        /// default value whenever the supplied parser function throws an
         /// exception and the parsed value otherwise.
         /// </summary>
 
@@ -74,20 +74,20 @@ namespace TryParsers
         /// <summary>
         /// Creates and returns a function that returns the result of a
         /// parser function and the result of a second function if the
-        /// parser function throws an exception of type 
+        /// parser function throws an exception of type
         /// <typeparamref name="TException"/>. Exceptions of any other type
         /// are propagated.
         /// </summary>
 
         public static Func<string, T> Make<T, TException>(
-            Func<string, T> parser, 
+            Func<string, T> parser,
             Func<TException, T> onError)
             where TException : Exception
         {
             return input =>
             {
                 try
-                {	        
+                {
                     return parser(input);
                 }
                 catch (TException e)
@@ -99,24 +99,24 @@ namespace TryParsers
 
         /// <summary>
         /// Creates and returns a function that returns the result of a
-        /// parser function and the result of a second or third function if 
-        /// the parser function throws an exception of type 
-        /// <typeparamref name="TException1"/> or 
-        /// <typeparamref name="TException2"/>, respectively. 
+        /// parser function and the result of a second or third function if
+        /// the parser function throws an exception of type
+        /// <typeparamref name="TException1"/> or
+        /// <typeparamref name="TException2"/>, respectively.
         /// Exceptions of any other type are propagated.
         /// </summary>
 
         public static Func<string, T> Make<T, TException1, TException2>(
-            Func<string, T> parser, 
+            Func<string, T> parser,
             Func<TException1, T> onError1,
-            Func<TException2, T> onError2) 
-            where TException1 : Exception 
+            Func<TException2, T> onError2)
+            where TException1 : Exception
             where TException2 : Exception
         {
             return input =>
             {
                 try
-                {	        
+                {
                     return parser(input);
                 }
                 catch (TException1 e)
@@ -132,27 +132,27 @@ namespace TryParsers
 
         /// <summary>
         /// Creates and returns a function that returns the result of a
-        /// parser function and the result of a second, third or fourth 
-        /// function if  the parser function throws an exception of type 
-        /// <typeparamref name="TException1"/> or 
-        /// <typeparamref name="TException2"/> or 
-        /// <typeparamref name="TException3"/>, respectively. 
+        /// parser function and the result of a second, third or fourth
+        /// function if  the parser function throws an exception of type
+        /// <typeparamref name="TException1"/> or
+        /// <typeparamref name="TException2"/> or
+        /// <typeparamref name="TException3"/>, respectively.
         /// Exceptions of any other type are propagated.
         /// </summary>
 
         public static Func<string, T> Make<T, TException1, TException2, TException3>(
-            Func<string, T> parser, 
+            Func<string, T> parser,
             Func<TException1, T> onError1,
             Func<TException2, T> onError2,
-            Func<TException3, T> onError3) 
-            where TException1 : Exception 
+            Func<TException3, T> onError3)
+            where TException1 : Exception
             where TException2 : Exception
             where TException3 : Exception
         {
             return input =>
             {
                 try
-                {	        
+                {
                     return parser(input);
                 }
                 catch (TException1 e)
